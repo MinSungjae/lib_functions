48a589c24e7f75a7f70c80ada474edc48fcc05ca
SungjaeMin <msj5826@gmail.com>
2025-04-09 02:05:30 +0900

    update

--- include/lib_functions/iostream_lib.h
+++ include/lib_functions/iostream_lib.h
@@ -2,8 +2,11 @@
 #define __IOSTREAM_LIB_H__
 
 #include <ostream>
+#include <sstream>
+#include <vector>
 
-#define DEBUG
+// #define DEBUG
+
 #ifdef DEBUG
     #define DEBUG_COUT(OUT) std::cout << OUT
     #define DEBUG_CERR(ERR) std::cerr << ERR
@@ -16,11 +19,20 @@ namespace Color {
     enum Code {
         FG_RED      = 31,
         FG_GREEN    = 32,
+        FG_YELLOW   = 33,
         FG_BLUE     = 34,
+        FG_MAGENTA  = 35,
+        FG_CYAN     = 36,
+        FG_WHITE    = 37,
+
         FG_DEFAULT  = 39,
         BG_RED      = 41,
         BG_GREEN    = 42,
+        BG_YELLOW   = 43,
         BG_BLUE     = 44,
+        BG_MAGENTA  = 45,
+        BG_CYAN     = 46,
+        BG_WHITE    = 47,
         BG_DEFAULT  = 49,
 
         BOLD_ON = 1,
@@ -39,4 +51,13 @@ namespace Color {
     };
 }
 
+template<typename T>
+std::string printStdVectorRow(const std::vector<T>& vec, const std::string& separator = " ") {
+    std::ostringstream oss;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        oss << vec[i];
+        if (i != vec.size() - 1) oss << separator;
+    }
+    return oss.str();
+}
 #endif
